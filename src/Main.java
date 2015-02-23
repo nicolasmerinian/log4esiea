271a47a3d63a4e0d39790a727a43e0e5163bab52
Nicolas Merinian <nicolas.merinian@gmail.com>
2015-02-23 11:13:51 +0100

    Modif package Logger

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import fr.esiea.log4esiea.Logger.Logger;
+
 
 public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hello world!");
+		Logger log = Logger.getLogger(Main.class);
+		Logger log2 = Logger.getLogger(Logger.class);
+		System.out.println(log == log2);
+		System.out.println(log.equals(log2));
 	}
 
 }
