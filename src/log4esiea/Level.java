d5e1cb522cdf6ba98a174a5893939cc9ad75b75b
Tino Nguyen <qttino@gmail.com>
2015-02-23 10:31:22 +0100

    Ajout de level.java

---
+++ src/log4esiea/Level.java
@@ -0,0 +1,18 @@
+package log4esiea;
+
+public enum Level {
+	
+	//off, trace, all, debug, info, warn, error, fatal
+	OFF(0), TRACE(1), ALL(2), DEBUG(3), INFO(4), WARN(5), ERROR(6), FATAL(7);
+	
+	private int level;
+	
+	Level(int level){
+		this.level = level;
+	}
+	
+	public int getLevel(){
+		return level;
+	}
+
+}
