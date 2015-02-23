0ddfb71477aa3b0173bc040f0d65b4ee38780c57
Nicolas Merinian <nicolas.merinian@gmail.com>
2015-02-23 09:57:48 +0100

    Modif de Logger

--- src/Logger/Logger.java
+++ src/Logger/Logger.java
@@ -1,5 +1,24 @@
 package Logger;
 
 public class Logger {
+	private static Logger logger;
+	
+	public Logger() {
+		super();
+	}
+
+	public static Logger getLogger() {
+		return logger;
+	}
+
+	public static void setLogger(Logger logger) {
+		Logger.logger = logger;
+	}
+
+	@Override
+	public String toString() {
+		return "Logger []";
+	}
+	
 	
 }
