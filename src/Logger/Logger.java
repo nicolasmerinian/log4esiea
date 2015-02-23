042d4605221ba0c5c750f29b5105398cc149d4e8
Nicolas Merinian <nicolas.merinian@gmail.com>
2015-02-23 10:29:41 +0100

    Logger test unicité 2

--- src/Logger/Logger.java
+++ src/Logger/Logger.java
@@ -1,7 +1,10 @@
 package Logger;
 
+import java.util.ArrayList;
+
 public class Logger {
 	private static Logger logger;
+	public ArrayList appenders;
 	
 	public Logger() {
 		super();
@@ -18,6 +21,32 @@ public class Logger {
 	@Override
 	public String toString() {
 		return "Logger []";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((appenders == null) ? 0 : appenders.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Logger other = (Logger) obj;
+		if (appenders == null) {
+			if (other.appenders != null)
+				return false;
+		} else if (!appenders.equals(other.appenders))
+			return false;
+		return true;
 	}
 	
 	
