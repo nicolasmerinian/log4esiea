3a847b84b2beb86da3baa3fa916f21c550d49d6b
Yassinge <tino.esiea@gmail.com>
2015-02-27 00:57:20 +0100

    fichier Formatter.java ajout commentaires jolis

--- src/fr/esiea/log4esiea/formatter/Formatter.java
+++ src/fr/esiea/log4esiea/formatter/Formatter.java
@@ -4,8 +4,32 @@ import fr.esiea.log4esiea.Level;
 
 
 public interface Formatter {
+	
+	/**
+	 * 
+	 * @param level
+	 */
 	public void setLevel(Level level);
+	
+	/**
+	 * 
+	 * @param format
+	 */
 	public void setFormat(String format);
+	
+	/**
+	 * 
+	 * @return
+	 */
 	public String getFormat();
+	
+	/**
+	 * 
+	 * @param message
+	 * @param className
+	 * @param methodName
+	 * @param lineNumber
+	 * @return
+	 */
 	public String format(String message, String className, String methodName, int lineNumber);
 }
