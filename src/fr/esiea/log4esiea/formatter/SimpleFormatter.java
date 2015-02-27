9d411078560210fd14a7b94f48920201893bd34c
Yassinge <tino.esiea@gmail.com>
2015-02-27 02:45:35 +0100

    modif simpleformatter.format()

--- src/fr/esiea/log4esiea/formatter/SimpleFormatter.java
+++ src/fr/esiea/log4esiea/formatter/SimpleFormatter.java
@@ -1,6 +1,11 @@
 package fr.esiea.log4esiea.formatter;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 import fr.esiea.log4esiea.Level;
+import fr.esiea.log4esiea.utils.MyDate;
+import fr.esiea.log4esiea.utils.Utils;
 
 public class SimpleFormatter implements Formatter {
 
@@ -44,7 +49,53 @@ public class SimpleFormatter implements Formatter {
 
 	@Override
 	public String format(String message, String className, String methodName, int lineNumber) {
-		return this.getLevel() + " " + className + "." + methodName + " l." + lineNumber + " : " + message;
+		StringBuilder formattedMessage = new StringBuilder();
+	    MyDate date = Utils.getMyDate();
+		String keyString = "(dd|MM|yyyy|hh|mm|ss|L|C|D|r|x|\\(|\\)|\\[|\\]|:| |/|-|.)";
+		Pattern p = Pattern.compile(keyString);
+		Matcher m = p.matcher(this.format);
+		while (m.find()) {
+		    String matched = m.group(1);
+		    switch (matched) {
+		    case "dd":
+		    	formattedMessage.append(date.getDay());
+		    	break;
+		    case "MM":
+		    	formattedMessage.append(date.getMonth());
+		    	break;
+		    case "yyyy":
+		    	formattedMessage.append(date.getYear());
+		    	break;
+		    case "hh":
+		    	formattedMessage.append(date.getHour());
+		    	break;
+		    case "mm":
+		    	formattedMessage.append(date.getMinute());
+		    	break;
+		    case "ss":
+		    	formattedMessage.append(date.getSecond());
+		    	break;
+		    case "L":
+		    	formattedMessage.append(this.level.name());
+		    	break;
+		    case "C":
+		    	formattedMessage.append(className);
+		    	break;
+		    case "D":
+		    	formattedMessage.append(methodName);
+		    	break;
+		    case "r":
+		    	formattedMessage.append(lineNumber);
+		    	break;
+		    case "x":
+		    	formattedMessage.append(message);
+		    	break;
+		    default:
+		    	formattedMessage.append(matched);
+		    	break;
+		    }
+		}
+		return formattedMessage.toString();
 	}
 
 	
