242594b416120db13fa94335f14c441ef85913e2
Yassinge <tino.esiea@gmail.com>
2015-02-27 01:00:23 +0100

    création fichier SimpleFormatter.java

---
+++ src/fr/esiea/log4esiea/formatter/SimpleFormatter.java
@@ -0,0 +1,32 @@
+package fr.esiea.log4esiea.formatter;
+
+import fr.esiea.log4esiea.Level;
+
+public class SimpleFormatter implements Formatter {
+
+	@Override
+	public void setLevel(Level level) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setFormat(String format) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public String getFormat() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String format(String message, String className, String methodName,
+			int lineNumber) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
